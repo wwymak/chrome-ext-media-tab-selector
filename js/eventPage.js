6cf7212283587d14869962687e21f5a3d1554333
WingYee Mak <wwymak@gmail.com>
2015-10-16 23:43:16 +0100

    highlighting audio page now working

--- js/eventPage.js
+++ js/eventPage.js
@@ -6,4 +6,14 @@ chrome.browserAction.onClicked.addListener(tabsQuery);
 
 function tabsQuery(){
     console.log("tabs query")
+    chrome.tabs.query({audible: true}, function(tabs) {
+        console.log(tabs)
+        tabs.forEach(function(item){
+            chrome.tabs.update(item.id, {
+                highlighted: true,
+                muted: true
+            })
+        })
+
+    })
 }
