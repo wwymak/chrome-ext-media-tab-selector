0868cd7a7caac4b15bdba3a4412fb10e643e7034
WingYee Mak <wwymak@gmail.com>
2015-10-18 23:46:49 +0100

    some more bits

--- js/eventPage.js
+++ js/eventPage.js
@@ -8,12 +8,41 @@ function tabsQuery(){
     console.log("tabs query")
     chrome.tabs.query({audible: true}, function(tabs) {
         console.log(tabs)
+        //check if tab window id = current window Id
+        //if yes higghlihgt the tab
+        //if not-- tabs.move to current window, then hightlihgt
         tabs.forEach(function(item){
+            var audioTabWindowID = item.windowID;
+            //checkCurrTabWindow(audioTabWindowID, )
             chrome.tabs.update(item.id, {
                 highlighted: true,
-                muted: true
+                //muted: true
             })
         })
 
     })
 }
+
+function checkCurrTabWindow(audibleTabWindowID, callback){
+    chrome.tabs.getCurrent(function(tab){
+        console.log(tab);
+        if(tab.windowID != audibleTabWindowID){
+            //move the window to top, and highlight audlbe tab
+            callback(tab.windowID);
+        }
+    })
+}
+
+function highlightTab(tabID){
+    chrome.tabs.update(item.id, {
+        highlighted: true,
+        //muted: true
+    })
+}
+
+function moveToCurrWindow(tabID, currWindowID, callback){
+    chrome.tabs.move(tabID, {windowId: currWindowID, index: 0}, callback)
+}
+
+
+//chrome.tabs.move(integer or array of integer tabIds, object moveProperties, function callback)
